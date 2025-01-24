9f512903766b018d56c7f20fa7b77f115d39be17
Nick <rudbsm@gmail.com>
2025-01-24 19:24:10 +0300

    Init

--- svelte.config.ts
+++ svelte.config.ts
@@ -1,14 +1,18 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-export default {
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: vitePreprocess(),
+
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
+			fallback: 'app.html'
 		}),
 		paths: {
-			base: process.env.GITHUB_REPOSITORY ? '/' : ''
+			base: process.env.NODE_ENV === 'production' ? '/skymosaic' : ''
 		}
 	}
 };
+
+export default config;

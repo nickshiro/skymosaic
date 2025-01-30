5d208b8fa43cf9c4b888f0914c3ad03a5c8ba88a
Nick <rudbsm@gmail.com>
2025-01-30 22:12:46 +0300

    Fixing zoom

--- src/widgets/paper/Paper.svelte
+++ src/widgets/paper/Paper.svelte
@@ -14,20 +14,20 @@
 >
 	<div class="absolute bottom-0 left-0 h-[35%] w-full items-center">
 		<h1
-			class="mx-auto max-w-[80%] break-words text-center font-primary text-[20px]"
+			class="mx-auto max-w-[80%] break-words text-center font-primary text-[2vmin]"
 			style={`color: ${$paperContext.text}`}
 		>
 			{$paperContext.header}
 		</h1>
 		<p
-			class="mx-auto max-w-[80%] break-words text-center font-primary text-[20px]"
+			class="mx-auto max-w-[80%] break-words text-center font-primary text-[2vmin]"
 			style={`color: ${$paperContext.text}`}
 		>
 			{$paperContext.subtitle}
 		</p>
 	</div>
 	<div class="absolute bottom-0 left-0 h-[10%] w-full items-center">
-		<p class="text-center font-primary text-[14px]" style={`color: ${$paperContext.text}`}>
+		<p class="text-center font-primary text-[1.5vmin]" style={`color: ${$paperContext.text}`}>
 			{$paperContext.date.toLocaleUpperCase()}
 			{$paperContext.time.toLocaleUpperCase()}
 		</p>

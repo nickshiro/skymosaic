ba1a4577617668720cb1841d2be9b154ffa73500
Nick <rudbsm@gmail.com>
2025-01-30 22:07:32 +0300

    Fixing paper

--- src/widgets/paper/Paper.svelte
+++ src/widgets/paper/Paper.svelte
@@ -12,18 +12,24 @@
 	id="paper"
 	style={`background-color: ${$paperContext.background}`}
 >
-	<div class="absolute bottom-0 left-0 h-[30%] w-full items-center">
-		<h1 class="text-center font-primary" style={`color: ${$paperContext.text}`}>
+	<div class="absolute bottom-0 left-0 h-[35%] w-full items-center">
+		<h1
+			class="mx-auto max-w-[80%] break-words text-center font-primary text-[20px]"
+			style={`color: ${$paperContext.text}`}
+		>
 			{$paperContext.header}
 		</h1>
-		<p class="text-center font-primary" style={`color: ${$paperContext.text}`}>
+		<p
+			class="mx-auto max-w-[80%] break-words text-center font-primary text-[20px]"
+			style={`color: ${$paperContext.text}`}
+		>
 			{$paperContext.subtitle}
 		</p>
 	</div>
 	<div class="absolute bottom-0 left-0 h-[10%] w-full items-center">
-		<p class="text-center font-primary" style={`color: ${$paperContext.text}`}>
-			{$paperContext.date}
-			{$paperContext.time}
+		<p class="text-center font-primary text-[14px]" style={`color: ${$paperContext.text}`}>
+			{$paperContext.date.toLocaleUpperCase()}
+			{$paperContext.time.toLocaleUpperCase()}
 		</p>
 	</div>
 </div>
